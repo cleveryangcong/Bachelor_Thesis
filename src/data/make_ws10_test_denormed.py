8cfaabb278bb441a39a393908db857cbc01d5b24
Delong <delongchen10062002@gmail.com>
2023-05-24 13:54:52 +0200

    Add make_ws10_test_denormed.py file

---
+++ src/data/make_ws10_test_denormed.py
@@ -0,0 +1,108 @@
+# Basics
+import numpy as np
+import time
+
+# Data
+import xarray as xr
+import h5py
+
+# Helpful
+from tqdm import tqdm
+
+# My Methods
+import data.raw.load_data_raw as ldr
+
+
+def denormalize(mean, std, x):
+    denormalized = (x * std) + mean
+    return denormalized
+
+
+def main():
+
+    # Define path and file names for the h5 file to be created
+    path = "/Data/Delong_BA_Data/mean_ens_std_denorm/ws10_test_denorm.h5"
+    f = h5py.File(path, "a")
+    name_test = "ws10_test"
+    name_truth = "ws10_test_truth"
+
+    # load global means and stds
+    means = np.load(
+        "/mnt/sda/Data2/fourcastnet/data/stats_v0/global_means.npy"
+    ).flatten()[[0, 1, 2, 5, 14]]
+    stds = np.load(
+        "/mnt/sda/Data2/fourcastnet/data/stats_v0/global_stds.npy"
+    ).flatten()[[0, 1, 2, 5, 14]]
+
+    # Load raw data for the years 2018-2021
+    # process one year at a time
+    dat_raw = ldr.load_data_raw()[4]  # load data for the year
+    n_days = dat_raw.predictions.shape[0]  # get number of days
+
+    # Create the datasets within the h5 file for 'train' and 'truth' data
+    # Create them once, before entering the forecast_date loop
+    if name_test in f:
+        del f[name_test]  # delete the dataset if it already exists
+    if name_truth in f:
+        del f[name_truth]  # delete the dataset if it already exists
+
+    test = f.create_dataset(
+        name_test,
+        (n_days, *dat_raw.predictions.isel(var=0, forecast_date=0).shape),
+        dtype=np.float32,
+        compression="gzip",
+        compression_opts=9,
+    )
+    truth = f.create_dataset(
+        name_truth,
+        (n_days, *dat_raw.ground_truth.isel(var=0, forecast_date=0).shape),
+        dtype=np.float32,
+        compression="gzip",
+        compression_opts=9,
+    )
+
+    for forecast_date in tqdm(range(n_days)):
+        # Compute the magnitude (absolute value) of wind speed predictions and truths
+        u10_year_date_pred = denormalize(
+            means[0],
+            stds[0],
+            dat_raw.predictions.isel(var=0, forecast_date=forecast_date),
+        )
+        v10_year_date_pred = denormalize(
+            means[1],
+            stds[1],
+            dat_raw.predictions.isel(var=1, forecast_date=forecast_date),
+        )
+        ws10_pred = np.hypot(u10_year_date_pred, v10_year_date_pred,)
+
+        u10_year_date_truth = denormalize(
+            means[0],
+            stds[0],
+            dat_raw.ground_truth.isel(var=0, forecast_date=forecast_date),
+        )
+        v10_year_date_truth = denormalize(
+            means[1],
+            stds[1],
+            dat_raw.ground_truth.isel(var=1, forecast_date=forecast_date),
+        )
+
+        ws10_tru = np.hypot(u10_year_date_truth, v10_year_date_truth,)
+
+        # Calculate mean and standard deviation of wind speed predictions
+        ws10_pred_mean = ws10_pred.mean(dim="ens")
+        ws10_pred_std = ws10_pred.std(dim="ens")
+
+        # Concatenate mean and standard deviation data along new 'mean_std' dimension
+        ws_test = xr.concat([ws10_pred_mean, ws10_pred_std], dim="mean_std")
+        ws_test = ws_test.transpose("lead_time", "lat", "lon", "mean_std")
+
+        # Populate the h5 file with the data
+        test[forecast_date, ...] = ws_test
+        truth[forecast_date, ...] = ws10_tru
+
+    # Close the h5 file
+    f.close()
+    
+    
+if __name__ == "__main__":
+    main()
