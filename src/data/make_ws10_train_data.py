b1cf5921472d4fddb0f3d81bfd85c0ec688d9a3c
Delong <delongchen10062002@gmail.com>
2023-05-24 12:09:39 +0200

    Change make_ws10_train_data to be more memory efficient by iterating over the years

--- src/data/make_ws10_train_data.py
+++ src/data/make_ws10_train_data.py
@@ -12,127 +12,61 @@ from tqdm import tqdm
 # My Methods
 import data.raw.load_data_raw as ldr
 
-def main():
-    """
-    Function to process and save wind speed prediction and ground truth data.
-    """
+# My Methods
+import data.raw.load_data_raw as ldr
 
-    # Initialize timer for performance tracking
-    start_time = time.time()
+def main():
 
     # Define path and file names for the h5 file to be created
     path = "/Data/Delong_BA_Data/Mean_ens_std/ws10_train.h5"
     f = h5py.File(path, "a")
     name_train = "ws10_train"
     name_truth = "ws10_truth"
-
+    
+    # Getting all n_days shapes of raw_data
+    n_days_shape = []
+    for year in (range(4)):
+        dat_raw = ldr.load_data_raw()[year]
+        n_days_shape.append(dat_raw.predictions.shape[0])
+    
     # Load raw data for the years 2018-2021
-    dat_raw = ldr.load_data_raw()[0:4]
-
-    # Compute the magnitude (absolute value) of wind speed predictions and truths
-    ws10_preds = []  # list to hold prediction data
-    ws10_truth = []  # list to hold ground truth data
+    # process one year at a time
     for year in tqdm(range(4)):
-        ws10_preds.append(
-            np.hypot(
-                dat_raw[year].predictions.isel(var=0),
-                dat_raw[year].predictions.isel(var=1),
-            )
-        )
-        ws10_truth.append(
-            np.hypot(
-                dat_raw[year].ground_truth.isel(var=0),
-                dat_raw[year].ground_truth.isel(var=1),
-            )
-        )
+        dat_raw = ldr.load_data_raw()[year]
 
-    # Calculate and concatenate mean and standard deviation of wind speed predictions
-    ws10_preds_mean = xr.concat(
-        [
-            ws10_preds[0].mean(dim="ens"),
-            ws10_preds[1].mean(dim="ens"),
-            ws10_preds[2].mean(dim="ens"),
-            ws10_preds[3].mean(dim="ens"),
-        ],
-        dim="forecast_date",
-    )
-    ws10_preds_std = xr.concat(
-        [
-            ws10_preds[0].std(dim="ens"),
-            ws10_preds[1].std(dim="ens"),
-            ws10_preds[2].std(dim="ens"),
-            ws10_preds[3].std(dim="ens"),
-        ],
-        dim="forecast_date",
-    )
+        # Compute the magnitude (absolute value) of wind speed predictions and truths
+        ws10_pred = np.hypot(dat_raw.predictions.isel(var=0), dat_raw.predictions.isel(var=1))
+        ws10_tru = np.hypot(dat_raw.ground_truth.isel(var=0), dat_raw.ground_truth.isel(var=1))
 
-    # Concatenate mean and standard deviation data along new 'mean_std' dimension
-    ws_train = xr.concat([ws10_preds_mean, ws10_preds_std], dim="mean_std")
-    ws_train = ws_train.transpose(
-        "forecast_date", "lead_time", "lat", "lon", "mean_std"
-    )
+        # Calculate mean and standard deviation of wind speed predictions
+        ws10_pred_mean = ws10_pred.mean(dim="ens")
+        ws10_pred_std = ws10_pred.std(dim="ens")
 
-    # Concatenate truth data
-    y_train = xr.concat(
-        [ws10_truth[0], ws10_truth[1], ws10_truth[2], ws10_truth[3]],
-        dim="forecast_date",
-    )
+        # Concatenate mean and standard deviation data along new 'mean_std' dimension
+        ws_train = xr.concat([ws10_pred_mean, ws10_pred_std], dim="mean_std")
+        ws_train = ws_train.transpose("forecast_date", "lead_time", "lat", "lon", "mean_std")
+        
+        
+        # Create the datasets within the h5 file for 'train' and 'truth' data
+        # append data to existing dataset, or create it if it doesn't exist
+        try:
+            train = f[name_train]
+        except:
+            train_shape = (sum(n_days_shape), *ws_train.shape[1:])  # adjust dimensions as per your data
+            train = f.create_dataset(name_train, shape=train_shape, dtype=np.float32, compression="gzip", compression_opts=9)
+        try:
+            truth = f[name_truth]
+        except:
+            truth_shape = (sum(n_days_shape), *ws10_tru.shape[1:])
+            truth = f.create_dataset(name_truth, shape=truth_shape, dtype=np.float32, compression="gzip", compression_opts=9)
 
-    # Extract shape of the data
-    n_days, n_lead_times, lat, long, mean_var = ws_train.shape
-
-    # Calculate elapsed time and print it
-    half_time = time.time()
-    time_difference_half = half_time - start_time
-    hours = int(time_difference_half // 3600)
-    minutes = int((time_difference_half % 3600) // 60)
-    seconds = int(time_difference_half % 60)
-    formatted_time_half = f" finished concatenation in:{hours} hours, {minutes} minutes, {seconds} seconds"
-    print(f"{formatted_time_half}")
-
-    # Create the datasets within the h5 file for 'train' and 'truth' data
-    try:
-        train = f.create_dataset(
-            name_train,
-            shape=(n_days, n_lead_times, lat, long, mean_var),
-            dtype=np.float32,
-            compression="gzip",
-            compression_opts=9,
-        )
-    except:
-        del f[name_train]  # if dataset already exists, delete it
-        train = f.create_dataset(
-            name_train,
-            shape=(n_days, n_lead_times, lat, long, mean_var),
-            dtype=np.float32,
-            compression="gzip",
-            compression_opts=9,
-        )
-    try:
-        truth = f.create_dataset(
-            name_truth,
-            shape=(n_days, n_lead_times, lat, long),
-            dtype=np.float32,
-            compression="gzip",
-            compression_opts=9,
-        )
-    except:
-        del f[name_truth]  # if dataset already exists, delete it
-        truth = f.create_dataset(
-            name_truth,
-            shape=(n_days, n_lead_times, lat, long),
-            dtype=np.float32,
-            compression="gzip",
-            compression_opts=9,
-        )
-
-    # Populate the h5 file with the data
-    for i in range(n_days):
-        train[i, ...] = ws_train.loc[i, ...]
-        truth[i, ...] = y_train[i, ...]
+        # Populate the h5 file with the data
+        train[sum(n_days_shape[0:year]):sum(n_days_shape[0:(year + 1)]), ...] = ws_train
+        truth[sum(n_days_shape[0:year]):sum(n_days_shape[0:(year + 1)]), ...] = ws10_tru
 
     # Close the h5 file
     f.close()
+
     
     
     
