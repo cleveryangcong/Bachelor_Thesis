c7ffac63c71213adb9561f9c7095df26b43913c0
Delong <delongchen10062002@gmail.com>
2023-05-25 21:28:14 +0200

    Clean up load data denormed with GPT

--- data/processed/load_data_processed_denormed.py
+++ data/processed/load_data_processed_denormed.py
@@ -6,17 +6,23 @@ def load_data_all_train_proc_denorm(chunks=None):
     Args:
         chunks (dict): Chunk sizes to use for the Dask arrays.
     Returns:
-        list: processsed dataset of train variables from 2018 - 2021 
+        list: processed dataset of train variables from 2018 - 2021 
               order_var_names = ["u10", "v10", "t2m", "t850", "z500", "ws10"]
     """
     # Load all data with chunks
     path = "/Data/Delong_BA_Data/mean_ens_std_denorm/"
-    dat_train_u10 = xr.open_dataset(path + "u10_train_denorm.h5")
-    dat_train_v10 = xr.open_dataset(path + "v10_train_denorm.h5")
-    dat_train_t2m = xr.open_dataset(path + "t2m_train_denorm.h5")
-    dat_train_t850 = xr.open_dataset(path + "t850_train_denorm.h5")
-    dat_train_z500 = xr.open_dataset(path + "z500_train_denorm.h5")
-    dat_train_ws10 = xr.open_dataset(path + "ws10_train_denorm.h5")
+    dat_train_u10 = xr.open_dataset(path + "u10_train_denorm.h5", chunks=chunks)
+    dat_train_v10 = xr.open_dataset(path + "v10_train_denorm.h5", chunks=chunks)
+    dat_train_t2m = xr.open_dataset(path + "t2m_train_denorm.h5", chunks=chunks)
+    dat_train_t850 = xr.open_dataset(path + "t850_train_denorm.h5", chunks=chunks)
+    dat_train_z500 = xr.open_dataset(path + "z500_train_denorm.h5", chunks=chunks)
+    dat_train_ws10 = xr.open_dataset(path + "ws10_train_denorm.h5", chunks=chunks).rename_dims({
+        "phony_dim_0": "forecast_date",
+        "phony_dim_1": "lead_time",
+        "phony_dim_2": "lat",
+        "phony_dim_3": "lon",
+        "phony_dim_4": "mean_std",
+    })
     
     dat_train_all = [
         dat_train_u10,
@@ -27,17 +33,7 @@ def load_data_all_train_proc_denorm(chunks=None):
         dat_train_ws10
     ]
 
-    var_dict = {
-        "phony_dim_0": "forecast_date",
-        "phony_dim_1": "lead_time",
-        "phony_dim_2": "lat",
-        "phony_dim_3": "lon",
-        "phony_dim_4": "mean_std",
-    }
-
-    dat_all = [dat_train_u10, dat_train_v10, dat_train_t2m, dat_train_t850, dat_train_z500, dat_train_ws10.rename_dims(var_dict)]
-
-    return dat_all
+    return dat_train_all
 
 def load_data_u10_train_proc_denorm():
     '''
@@ -88,42 +84,38 @@ def load_data_ws10_train_proc_denorm():
 
 def load_data_all_test_proc_denorm(chunks=None):
     """
-    Load all variable train processed and denormed data and format dimensions
+    Load all variable test processed and denormed data and format dimensions
     Args:
         chunks (dict): Chunk sizes to use for the Dask arrays.
     Returns:
-        list: processsed dataset of train variables from 2018 - 2021 
+        list: processed dataset of test variables 
               order_var_names = ["u10", "v10", "t2m", "t850", "z500", "ws10"]
     """
     # Load all data with chunks
     path = "/Data/Delong_BA_Data/mean_ens_std_denorm/"
-    dat_train_u10 = xr.open_dataset(path + "u10_test_denorm.h5")
-    dat_train_v10 = xr.open_dataset(path + "v10_test_denorm.h5")
-    dat_train_t2m = xr.open_dataset(path + "t2m_test_denorm.h5")
-    dat_train_t850 = xr.open_dataset(path + "t850_test_denorm.h5")
-    dat_train_z500 = xr.open_dataset(path + "z500_test_denorm.h5")
-    dat_train_ws10 = xr.open_dataset(path + "ws10_test_denorm.h5")
-    
-    dat_train_all = [
-        dat_train_u10,
-        dat_train_v10,
-        dat_train_t2m,
-        dat_train_t850,
-        dat_train_z500,
-        dat_train_ws10
-    ]
-
-    var_dict = {
+    dat_test_u10 = xr.open_dataset(path + "u10_test_denorm.h5", chunks=chunks)
+    dat_test_v10 = xr.open_dataset(path + "v10_test_denorm.h5", chunks=chunks)
+    dat_test_t2m = xr.open_dataset(path + "t2m_test_denorm.h5", chunks=chunks)
+    dat_test_t850 = xr.open_dataset(path + "t850_test_denorm.h5", chunks=chunks)
+    dat_test_z500 = xr.open_dataset(path + "z500_test_denorm.h5", chunks=chunks)
+    dat_test_ws10 = xr.open_dataset(path + "ws10_test_denorm.h5", chunks=chunks).rename_dims({
         "phony_dim_0": "forecast_date",
         "phony_dim_1": "lead_time",
         "phony_dim_2": "lat",
         "phony_dim_3": "lon",
         "phony_dim_4": "mean_std",
-    }
+    })
+    
+    dat_test_all = [
+        dat_test_u10,
+        dat_test_v10,
+        dat_test_t2m,
+        dat_test_t850,
+        dat_test_z500,
+        dat_test_ws10
+    ]
 
-    dat_all = [dat_train_u10, dat_train_v10, dat_train_t2m, dat_train_t850, dat_train_z500, dat_train_ws10.rename_dims(var_dict)]
-
-    return dat_all
+    return dat_test_all
 
 def load_data_u10_test_proc_denorm():
     '''
