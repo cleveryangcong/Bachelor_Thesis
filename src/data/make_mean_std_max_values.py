ff27d234d8dcedf12678cea799ebbff1040573e0
Delong <delongchen10062002@gmail.com>
2023-05-31 21:23:29 +0200

    Add tqdm to function

--- src/data/make_mean_std_max_values.py
+++ src/data/make_mean_std_max_values.py
@@ -1,12 +1,13 @@
 import numpy as np
 import data.processed.load_data_processed_denormed as ldpd
 from src.utils.data_split import *
+from tqdm import tqdm
 
 def make_mean_std_max(dat_var_lead_all):
     path = "/Data/Delong_BA_Data/mean_std_max_values/denorm/"
     mean_max = np.zeros([6, 31])
     std_max = np.zeros([6, 31])
-    for var in range(6):
+    for var in tqdm(range(6)):
         for lead_time in range(31):
             mean_max[var, lead_time] = (
                 dat_var_lead_all[var][lead_time].isel(mean_std=0).max().values
