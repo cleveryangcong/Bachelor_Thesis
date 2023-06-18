12dbae3647df3ca9d58ae21ce5db79b2866c049e
Delong <delongchen10062002@gmail.com>
2023-06-18 18:58:56 +0200

    Fix loading crps val and test, add notebook for checking differences

--- src/models/CRPS_baseline/CRPS_load.py
+++ src/models/CRPS_baseline/CRPS_load.py
@@ -2,24 +2,28 @@ import os
 import numpy as np
 import fnmatch
 
+import re
+import os
+import numpy as np
+
 def crps_load_lead_lat_lon(var_name):
     '''
     Args:
-        path (string): path to the directory that contains the numpy arrays
         var_name (string): the variable name used in the numpy files
     Returns:
         list of numpy arrays
     '''
     path = "/Data/Delong_BA_Data/scores/crps_benchmark_scores/"
+
     # Create the file pattern based on the variable name
-    file_pattern = f'{var_name}_lead_*_scores.npy'
+    file_pattern = re.compile(f'{var_name}_lead_\d{{1,2}}_scores\.npy$')
 
     # List all files in the directory
     files = os.listdir(path)
 
     # Filter the list to only include .npy files that match the file pattern
-    npy_files = [file for file in files if fnmatch.fnmatch(file, file_pattern)]
-    
+    npy_files = [file for file in files if file_pattern.match(file)]
+
     # Sort the file list based on the lead time
     npy_files.sort(key=lambda file: int(file.split('_')[2]))
 
