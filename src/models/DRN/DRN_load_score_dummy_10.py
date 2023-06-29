a558493ac138422385e79a83504cd9bab4e5794a
Delong <delongchen10062002@gmail.com>
2023-06-29 17:03:39 +0200

    Add load functions for scores and preds

---
+++ src/models/DRN/DRN_load_score_dummy_10.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import fnmatch
+
+def DRN_load_score_dummy_10(var_name):
+    '''
+    Args:
+        path (string): path to the directory that contains the numpy arrays
+        var_name (string): the variable name used in the numpy files
+    Returns:
+        list of numpy arrays
+    '''
+    path = "/Data/Delong_BA_Data/scores/DRN_scores_dummy_10/"
+    # Create the file pattern based on the variable name
+    file_pattern = f'DRN_{var_name}_lead_time_*_dummy_scores.npy'
+
+    # List all files in the directory
+    files = os.listdir(path)
+
+    # Filter the list to only include .npy files that match the file pattern
+    npy_files = [file for file in files if fnmatch.fnmatch(file, file_pattern)]
+    
+    # Sort the file list based on the lead time
+    npy_files.sort(key=lambda file: int(file.split('_')[4]))
+    
+    # Load each .npy file and store it in a list
+    arrays = [np.load(os.path.join(path, file)) for file in npy_files]
+
+    return arrays
